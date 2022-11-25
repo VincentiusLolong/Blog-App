652a835be47ce59fc79409188f1494d26207c662
VincentiusLolong <vincent.lolong3@gmail.com>
2022-11-25 07:16:03 +0700

    change struct models

--- models/user_models.go
+++ models/user_models.go
@@ -8,15 +8,19 @@ type signIn struct {
 	Password string             `json:"password,omitempty" validate:"required"`
 }
 type User struct {
-	Id    primitive.ObjectID `json:"id,omitempty"`
-	Name  string             `json:"name,omitempty" validate:"required"`
-	Age   string             `json:"age,omitempty" validate:"required"`
-	Orgs  string             `json:"orgs,omitempty" validate:"required"`
-	About string             `json:"about,omitempty" validate:"required"`
+	Id       primitive.ObjectID `json:"id,omitempty"`
+	Email    string             `json:"email,omitempty" validate:"required"`
+	Password string             `json:"password,omitempty" validate:"required"`
+	Name     string             `json:"name,omitempty" validate:"required"`
+	Age      string             `json:"age,omitempty" validate:"required"`
+	Orgs     string             `json:"orgs,omitempty" validate:"required"`
+	About    string             `json:"about,omitempty" validate:"required"`
+	Gender   string             `json:"gender,omitempty" validate:"required"`
 }
 
 type AllContents struct {
 	ContentId primitive.ObjectID `json:"contentid,omitempty"`
+	User_id   string             `json:"user_id,omitempty"`
 	Title     string             `json:"title,omitempty" validate:"required"`
 	Tags      string             `json:"tag,omitempty" validate:"required"`
 	Type      string             `json:"type,omitempty" validate:"required"`
