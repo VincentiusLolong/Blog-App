230530138d63093dde3ffa5715218621e25c5b8a
Vincentius Tri Nugroho Kuswiharto Lolong <vincent.lolong3@gmail.com>
2022-12-19 18:04:58 +0700

    add (email check) in CreateUser

--- controllers/repo/userDB.go
+++ controllers/repo/userDB.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fiber-mongo-api/configs"
 	"fiber-mongo-api/models"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -24,13 +25,17 @@ func CreateUserDB(userDb models.User, a context.Context) (*mongo.InsertOneResult
 		About:    userDb.About,
 		Gender:   userDb.Gender,
 	}
-
+	var user *models.User
+	err := userCollection.FindOne(a, bson.M{"email": userDb.Email}).Decode(&user)
+	str := fmt.Sprintf("Email (%v) Already Registered", user.Email)
+	if err == nil {
+		return nil, errors.New(str)
+	}
 	result, err := userCollection.InsertOne(a, newUser)
 	if err != nil {
-		return nil, errors.New("email already exists")
-	} else {
-		return result, nil
+		return nil, errors.New("err: 505")
 	}
+	return result, nil
 }
 
 func SignInDB(userLogin models.Login, a context.Context) (*models.User, error) {
@@ -53,5 +58,18 @@ func UserPorfileDB(a context.Context, user models.User, id string) error {
 		return errors.New("cant find the account")
 	} else {
 		return nil
+	}
+}
+
+func UserEdit(a context.Context, str string, bsondata primitive.M) (*mongo.UpdateResult, error) {
+	objId, _ := primitive.ObjectIDFromHex(str)
+	filter := bson.D{{Key: "id", Value: objId}}
+	update := bson.D{{Key: "$set", Value: bsondata}}
+
+	result, err := userCollection.UpdateOne(a, filter, update)
+	if err != nil {
+		return nil, errors.New("cant edit")
+	} else {
+		return result, nil
 	}
 }
